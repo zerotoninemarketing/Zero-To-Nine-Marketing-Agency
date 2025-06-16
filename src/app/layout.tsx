b5e89fa9cf06d033dae681dca7871a46ce996466
zerotoninemarketing <zerotoninemarketing@gmail.com>
2025-06-16 09:53:30 +0530

    Added Formspree to contact form and Analytics enabled

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
